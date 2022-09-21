c02aec6a1860fbf863c078d250222cc4995c753b
REVANTH-KARIAPPA <reavanthkariappa4@gmail.com>
2022-09-21 23:05:23 +0530

    ss

--- src/main/java/com/furniture/model/Currency.java
+++ src/main/java/com/furniture/model/Currency.java
@@ -5,10 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -20,6 +17,7 @@ public class Currency {
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     private int currencyId;
+    @Column(unique=true)
     private String currency;
     private String currencyName;
     private Double exchangeRate;
